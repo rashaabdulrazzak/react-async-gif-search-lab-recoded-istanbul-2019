505fdc60b6d8f7aca67ab4d191916cbddcc9ed7a
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-03-06 05:06:25 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -7,7 +7,6 @@ class GifList extends Component {
        {this.props.list.map(iimg=>{
                     return <li><img src={iimg.images.original.url} alt=""/></li>
                 })}
-      //{this.props.list}.map(item=>{return <li><img src={item.images.original.url} alt='image'/> </li>})
       </ul>
        
       </div>
